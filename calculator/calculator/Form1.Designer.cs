aafd7e1bbf137f511ac95835f7896d49f876fb96
545477 <5nvpvas@gmail.com>
2020-11-23 02:45:21 +0300

    Revert "calculator mod color back"
    This reverts commit 4d821d744441d55ea205c120b72599e1f5dd7f49.

--- calculator/calculator/Form1.Designer.cs
+++ calculator/calculator/Form1.Designer.cs
@@ -259,7 +259,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(366, 154);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.label1);
